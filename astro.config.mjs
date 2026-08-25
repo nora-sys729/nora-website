8554dcf1495f0049545e1101dcecb96438cfca7f
nora-sys729 <norawith.2001@gmail.com>
2026-08-25 23:55:20 +0700

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nora-sys729.github.io',
+  base: '/nora-website',
+})
